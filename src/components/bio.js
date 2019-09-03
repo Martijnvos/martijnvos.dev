dfc8f0e8d909c4a64f3071a997f73aec6e301e70
Martijn Vos <info@vosdevelopment.nl>
2019-09-03 18:46:43 +0200

    refactor: change personal picture

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,7 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/martijn-vos.jpg/" }) {
         childImageSharp {
           fixed(width: 50, height: 50) {
             ...GatsbyImageSharpFixed
@@ -54,12 +54,8 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        A software blog showcasing and documenting learnings. <br />
+        Made by <a href={`https://twitter.com/${social.twitter}`} target="_blank" rel="noopener noreferrer"><strong>{author}</strong></a>.
       </p>
     </div>
   )
