339cf5c122e07f674dbb353b3cabb74f3c8f6126
Martijn Vos <info@vosdevelopment.nl>
2019-09-04 23:25:34 +0200

    chore: remove Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,12 +48,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Martijn Vos Software Blog`,
