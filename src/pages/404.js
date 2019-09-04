8f4d9728bffde0148b6dc493a2d8ebe5d9bf0dde
Martijn Vos <info@vosdevelopment.nl>
2019-09-04 23:39:38 +0200

    refactor: update 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,8 +12,11 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>Post not Found</h1>
+        <p>
+          You just hit a post that doesn&#39;t exist... <br />
+          Get in contact if you have suggestions for future blog posts
+        </p>
       </Layout>
     )
   }
